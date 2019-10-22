70a197aa0877aeadd89adb65747c29b24637b512
91085566 <audianreinhard@gmail.com>
2019-10-22 13:59:37 -0700

    Initial commit

---
+++ src/dog.java
@@ -0,0 +1,50 @@
+public class dog
+{
+    //global variables
+    private String name, breed;
+    private int age;
+    private double weight;
+    private static int count;
+
+    //getters
+    public String getName()
+    {
+        return name;
+    } //end getName
+    public String getBreed()
+    {
+        return breed;
+    } //end getBreed
+    public int getAge()
+    {
+        return age;
+    } //end getAge
+    public double getWeight()
+    {
+        return weight;
+    } //end getWeight
+
+    //setters
+    public void setName()
+    {
+        name = this.name;
+    }//end setName
+    public void setBreed()
+    {
+        breed = this.breed;
+    }//end setBreed
+    public void setAge()
+    {
+        age = this.age;
+    }//end setAge
+    public void setWeight()
+    {
+        weight = this.weight;
+    }//end setWeight
+
+    //toString
+    public String toString() {
+        String output = "";
+        return output;
+    }
+}
---
+++ src/dogDriver.java
@@ -0,0 +1,7 @@
+public class dogDriver
+{
+    public static void main(String[] args)
+    {
+
+    }//end main
+}//end dogDriver
